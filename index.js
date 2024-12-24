5f2547599bab552962a33b70dc7885deb374aa48
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-12-24 19:52:02 +0530

    Initial file setup with mongoDB connection and .env used to secure

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i53p4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+// default route
+app.get("/", async (req, res) => {
+  res.send("Server running!!!!");
+});
+
+// listen
+app.listen(port, () => {
+  console.log(`server running at ${port}`);
+});
